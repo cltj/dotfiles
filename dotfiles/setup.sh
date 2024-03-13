b386f2f0ff46d13dccde05dda7f7a7fccd429387
cltj <tj@cloudlink.ai>
2024-03-13 12:14:07 +0100

    -t removed and readme upadted

--- dotfiles/setup.sh
+++ dotfiles/setup.sh
@@ -88,7 +88,7 @@ do
                 echo "$(date) - $command installed." | tee -a setuplog.txt
                 ;;
             "git-credential-manager")
-                curl -L "https://github.com/git-ecosystem/git-credential-manager/releases/download/v2.4.1/gcm-linux_amd64.2.4.1.deb" -O /tmp/gcmcore.deb && sudo dpkg -i /tmp/gcmcore.deb
+                wget "https://github.com/git-ecosystem/git-credential-manager/releases/download/v2.4.1/gcm-linux_amd64.2.4.1.deb" -O /tmp/gcmcore.deb && sudo dpkg -i /tmp/gcmcore.deb
                 sudo apt update
                 ;;
             # "poetry")
