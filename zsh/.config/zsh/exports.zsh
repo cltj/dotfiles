fe57f39d29a041d427cad5b53a5ca379f1eff5db
cltj <72233033+cltj@users.noreply.github.com>
2025-04-13 06:11:19 +0200

    Update exports.zsh

--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -4,7 +4,7 @@
 export DISPLAY=:0
 export WEZTERM_CONFIG_FILE=~/.wezterm.lua
 export PATH="$PATH:/opt/nvim-linux64/bin"
-export PATH="/home/tj/.local/bin/poetry:$PATH"
+export PATH="/home/cltj/.local/bin/poetry:$PATH"
 export PATH="$HOME/.local/bin:$PATH"
 export SAVEHIST=2000
 export HISTSIZE=1000
